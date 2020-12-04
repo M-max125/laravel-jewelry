bd483fd3e8d2856a361bb0b8ae3f20fcf416013e
M-max125 <c.marcu60@yahoo.com>
2020-12-04 21:51:22 +0200

    solving integrity constraint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,9 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(CategoriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
+    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
